283bb37a34d4da79005a8bb9cc07ccb480448be8
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-01-12 17:31:26 +0200

    Task 01 done with CSS

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,34 @@
 import './Profile.css'
 
-export default function App() {
+export const Profile = ({name, tag, location, image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png", stats}) => {
     return (
         <>
-        <p>Hello Dim-Dim</p>
+            <div className="profile">
+                <div className="description">
+                    <img
+                        src={image}
+                        alt="User avatar"
+                        className="avatar"
+                    />
+                    <p className="name">{name}</p>
+                    <p className="tag">@{tag}</p>
+                    <p className="location">{location}</p>
+                </div>
+                <ul className="stats">
+                    <li>
+                        <span className="label">Followers</span>
+                        <span className="value">{stats.followers}</span>
+                    </li>
+                    <li>
+                        <span className="label">Views</span>
+                        <span className="value">{stats.views}</span>
+                    </li>
+                    <li>
+                        <span className="label">Likes</span>
+                        <span className="value">{stats.likes}</span>
+                    </li>
+                </ul>
+            </div>
         </>
-    )
+    );
 }
