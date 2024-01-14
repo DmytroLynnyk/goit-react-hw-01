d72e348defa00996b7b6d9072f59620d91ae56f9
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-01-14 18:05:12 +0200

    Task 03 js withour css

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,22 @@
+import { TransactionHistoryItem } from '../TransactionHistoryItem/TransactionHistoryItem'
+
+export const TransactionHistory = ({items}) => {
+    return (
+        <table className="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map(item => (
+                    <tr key={item.id}>
+                        <TransactionHistoryItem item={item} />
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
